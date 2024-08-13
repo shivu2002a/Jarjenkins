aba5b7353e04b683a84d3dd51856c2626fb50ae9
shivu2002a <shivu.a.1945@gmail.com>
2024-08-13 10:18:40 +0530

    Initial

---
+++ src/test/java/com/shiva/AppTest.java
@@ -0,0 +1,20 @@
+package com.shiva;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        App ap = new App();
+        assertEquals("Hello world !!", ap.getGreeting());
+    }
+}
